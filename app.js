4d539da0753bfb5fccd98d7ef0546e33aa378b5f
Ahmed ali khan <159822322+James08923@users.noreply.github.com>
2024-02-22 00:14:01 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,108 @@
+// CHAPTER NO 1
+// Q1
+// alert("Error! Please enter a valid password.");
+
+
+// Q2
+// alert("Welcome to Js land...\nHappy Coding!");
+
+
+// Q3
+// alert("Welcome to Js land...");
+
+
+// Q4
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+
+//Q5
+// alert("Hello... i can run Js through my web browser's consal.");
+
+
+
+
+
+
+// CHAPTER NO 2
+//Q1
+// alert("Jhone Doe.");
+
+
+//Q2
+// alert("15 years old.");
+
+
+//Q3
+// alert("Certified Mobile Application Development.");
+
+
+//Q4
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+
+//Q5
+// alert("My email address is example@example.com.");
+
+
+//Q6
+alert("I am trying to learn the Book A smarter\nWay to learn JavaScript..");
+
+
+//Q7
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+
+
+
+
+// CHAPTER NO 3
+//Q1
+// alert("I am 15 years old.");
+
+
+// Q2
+// alert("You have visited this site 14 times");
+
+
+//Q3
+document.write("My birth year is 1990<br/>Data type of my ddeclared variable is number.");
+
+
+//Q4
+// document.write("John Doe ordered 5-Tshirt(s) on xyz clothing store.");
+
+
+
+
+
+
+//CHAPTER NO 4
+//Q1
+// alert("Ahmed");
+
+
+//Q2
+// alert("Ali Khan");
+
+
+//Q3
+// alert("ahmedalikhan182006@gmail.com");
+
+
+//Q4
+// alert("03160208269");
+
+
+//Q5
+// alert("ahmed12345");
+
+
+
+
+
+
+
+
+
+
